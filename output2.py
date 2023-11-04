28f4259db2075562a0e3a5adaabaf7a54d26dfae
17tyDM <takehiiragi@gmail.com>
2023-11-04 17:49:38 +0900

    commit5

--- output2.py
+++ output2.py
@@ -9,27 +9,31 @@ df = pd.read_csv('input2.csv', header=None, names=['生徒番号', '科目番号
 # 科目番号を科目名に変換
 df['科目名'] = df['科目番号'].map(subject_mapping)
 
-# 生徒番号ごとに科目別の合計、平均、成績、判定を計算
+# 生徒番号ごとに科目別の合計、平均を計算
+subject_totals = df.groupby(['生徒番号', '科目名'])['点数'].sum().unstack()
+subject_means = df.groupby(['生徒番号', '科目名'])['点数'].mean().unstack()
+
+# 生徒間での平均点の順位を計算
+student_rank = subject_means.mean(axis=1).rank(ascending=True).astype(int)
+
+# 生徒番号ごとに成績と判定を計算
 student_summary = {}
-for student_id, student_data in df.groupby('生徒番号'):
-    subject_totals = student_data.groupby('科目名')['点数'].sum()
-    subject_means = student_data.groupby('科目名')['点数'].mean()
-    
-    # 平均点を使って順位を計算
-    rank = subject_means.rank(ascending=False).astype(int)
-    
+for student_id, rank in zip(subject_totals.index, student_rank):
     grades = []
     judgments = []
-
-    for subject, total, mean, r in zip(subject_totals.index, subject_totals, subject_means, rank):
+    
+    for subject in subject_totals.columns:
+        total = subject_totals.loc[student_id][subject]
+        mean = subject_means.loc[student_id][subject]
+        
         # 順位から成績を決定
-        if r == 1:
+        if rank == 1:
             grade = 'A'
-        elif r <= 3:
+        elif rank <= 3:
             grade = 'B'
-        elif r <= 7:
+        elif rank <= 7:
             grade = 'C'
-        elif r <= 9:
+        elif rank <= 9:
             grade = 'D'
         else:
             grade = 'E'
@@ -37,14 +41,16 @@ for student_id, student_data in df.groupby('生徒番号'):
         # 判定を初期化
         judgment = '合格'
 
-        # 成績がDまたはEの場合は再テストまたは不合格
-        if grade in ('D', 'E'):
-            judgment = '再テスト' if student_data[student_data['科目名'] == subject]['点数'].lt(30).sum() >= 3 else '不合格'
+        # 判定の条件分岐
+        if (total <= 30).sum() >= 3 or grade in ('D'):
+            judgment = '再テスト'
+        elif (mean < 10).any() or grade in ('E'):
+            judgment = '不合格'
 
         grades.append(grade)
         judgments.append(judgment)
 
-    student_summary[student_id] = {subject: [total, mean, r, grade, judgment] for subject, total, mean, r, grade, judgment in zip(subject_totals.index, subject_totals, subject_means, rank, grades, judgments)}
+    student_summary[student_id] = {subject: [total, mean, rank, grade, judgment] for subject, total, mean, grade, judgment in zip(subject_totals.columns, subject_totals.loc[student_id], subject_means.loc[student_id], grades, judgments)}
 
 # 生徒番号ごとに成績と判定を出力
 for student_id, subjects in student_summary.items():
