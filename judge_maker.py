50f00896e33faf59662f950415b78d438ea761db
17tyDM <takehiiragi@gmail.com>
2023-11-18 17:52:57 +0900

    commit7

--- judge_maker.py
+++ judge_maker.py
@@ -16,7 +16,21 @@ def make_judge(grade:str, points:list[int])->int:
                 return 1
     elif grade not in ["A", "B", "C", "D", "E"]:
         raise Exception("graderにA～E以外の文字が入力されています")
-    elif len(points) != 10 or all(0 <= point <= 100 for point in points):
+    elif len(points) != 10 or not all(0 <= point <= 100 for point in points):
         raise Exception("pointsには0~100の数値10個をリスト型を入力してください")
     else:
         raise Exception("正しく入力されていません")
+    
+def test_make_judge_exercise_3():
+    """
+    スライド22ページ：課題
+    テスト内容：pointsの値に100.1の場合
+    """
+    try:
+        grade = "A"
+        points = [100.1, 100, 100, 100, 100, 100, 100, 100, 100, 100]
+        result = make_judge(grade, points)
+        assert False
+    except Exception as e:
+        assert e.args[0] == "正しく入力されていません"
+test_make_judge_exercise_3()
